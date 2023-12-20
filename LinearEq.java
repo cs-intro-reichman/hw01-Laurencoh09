4f574168ae70b035584a7f6e0bc81a8434e7150f
Lauren Cohen <154359581+Laurencoh09@users.noreply.github.com>
2023-12-20 22:39:54 +0200

    Update LinearEq.java

--- LinearEq.java
+++ LinearEq.java
@@ -7,14 +7,14 @@
 public class LinearEq {
    	public static void main (String[] args) {
 
-   		double a = Double.parseDouble(args[0]);
-   		double b = Double.parseDouble(args[1]);
-   		double c = Double.parseDouble(args[2]);
+   		double a =Double.parseDouble(args[0]);
+   		double b =Double.parseDouble(args[1]);
+   		double c =Double.parseDouble(args[2]);
         
 
    		System.out.println(a + " * " + "x" + " + " + b + " = " + c );
 
-   		double x = (c - b)/a;
+   		double x =((c - b)/a);
 
         System.out.println("x" + " = " + x);
 
