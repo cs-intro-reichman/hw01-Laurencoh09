8fa384cdff6ebd55a72fa0457221ded733d8ab0b
Lauren Cohen <154359581+Laurencoh09@users.noreply.github.com>
2023-12-20 22:37:11 +0200

    Update Coins.java

--- Coins.java
+++ Coins.java
@@ -5,9 +5,9 @@
 public class Coins {
 	public static void main(String[] args) {
 		
-    int a= Integer.parseInt(args[0]);
-    int b= a/25;
-    int c= a-(b*25);
+    int a =Integer.parseInt(args[0]);
+    int b =a/25;
+    int c =a-(b*25);
 
 System.out.println("Use " + b + " quarters " + "and " + c + " cents ");
 
