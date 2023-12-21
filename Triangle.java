22f560f18deee2f2ddb82abac306f6660847ca93
Lauren Cohen <154359581+Laurencoh09@users.noreply.github.com>
2023-12-21 13:11:08 +0200

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -3,6 +3,8 @@
  * This is known as the Triangle Inequality Theorem. 
  * Write a program that tests if three given integers form a triangle.
  */ 
+
+
 public class Triangle {
 	public static void main(String[] args) {
 		
