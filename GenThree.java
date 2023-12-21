c581bf843c7e1de88d9b876cdb685e1ad755172d
Lauren Cohen <154359581+Laurencoh09@users.noreply.github.com>
2023-12-21 13:10:30 +0200

    Update GenThree.java

--- GenThree.java
+++ GenThree.java
@@ -2,6 +2,7 @@
  * Generates three random integers, each in a given range [a,b),
  * prints them, and then prints the minimal number that was generated.
  */
+ 
 public class GenThree {
    	public static void main (String[] args) {
 
