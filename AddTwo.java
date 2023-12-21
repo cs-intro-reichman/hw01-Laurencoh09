cebb1ece8977b6dbc23a50c94ef52b6c063dcd73
Lauren Cohen <154359581+Laurencoh09@users.noreply.github.com>
2023-12-21 07:45:45 +0200

    Update AddTwo.java

--- AddTwo.java
+++ AddTwo.java
@@ -2,7 +2,7 @@
  * Adds two given integers and prints the result in a fancy way.
  */
 
-public class AddTwo{
+public class AddTwo {
    	public static void main (String[] args) {
 
    		int a =Integer.parseInt(args[0]);
